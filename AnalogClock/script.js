1687c959c9b05f21f0a2c684e3cd9ea59323ff28
KajaBraz <kajabraz@gmail.com>
2020-12-24 05:00:20 +0100

    clock's hands movement

--- AnalogClock/script.js
+++ AnalogClock/script.js
@@ -2,10 +2,23 @@ const hourHand = document.querySelector("#hour");
 const minuteHand = document.querySelector("#minute");
 const secondHand = document.querySelector("#second");
 
-let hrPosition=20;
-let minPosition=130;
-let secPosition=267;
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
 
-hourHand.style.transform="rotate("+hrPosition+"deg)"
-minuteHand.style.transform="rotate("+minPosition+"deg)"
-secondHand.style.transform="rotate("+secPosition+"deg)"
+let secPosition = sec * 360 / 60;
+let minPosition = (min * 360 / 60) + (secPosition / 60);
+let hrPosition = (hr * 360 / 12) + (minPosition / 12);
+
+function runTheClock() {
+    hrPosition += (3 / 360)
+    minPosition += (6 / 60)
+    secPosition += 6
+
+    hourHand.style.transform = "rotate(" + hrPosition + "deg)";
+    minuteHand.style.transform = "rotate(" + minPosition + "deg)";
+    secondHand.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runTheClock, 1000)
