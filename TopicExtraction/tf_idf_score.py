1a0518eba19ff186372ba36c4623a5b55ac56b27
KajaBraz <kajabraz@gmail.com>
2021-01-17 21:53:55 +0100

    printing words with the highest score

--- TopicExtraction/tf_idf_score.py
+++ TopicExtraction/tf_idf_score.py
@@ -1,5 +1,6 @@
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 import numpy as np
+import pandas as pd
 
 
 def calculate_tf_idf(texts: [str]):
@@ -7,19 +8,28 @@ def calculate_tf_idf(texts: [str]):
     counts = vectorizer.fit_transform(texts)
     transformer = TfidfTransformer(norm=None)
     tfidf_scores_transformed = transformer.fit_transform(counts)
-    return tfidf_scores_transformed
+    names = vectorizer.get_feature_names()
+    return tfidf_scores_transformed, names
 
 
 def calculate_tf_idf2(texts: [str]):
     vectorizer = TfidfVectorizer(norm=None)
     tfidf_scores = vectorizer.fit_transform(texts)
-    return tfidf_scores
+    names = vectorizer.get_feature_names()
+    return tfidf_scores, names
 
 
 def check_tfidf_similarity(scores1, scores2):
     if np.allclose(scores1.todense(), scores2.todense()):
         return True
     return False
+
+
+def print_results(scores, feature_names, texts_num):
+    column_names = [f'Text n.{x}' for x in range(1, texts_num + 1)]
+    df = pd.DataFrame(scores.T.todense(), index=feature_names, columns=column_names)
+    for text_num in range(1, texts_num + 1):
+        print(df[[f'Text n.{text_num}']].idxmax())
 
 
 if __name__ == '__main__':
