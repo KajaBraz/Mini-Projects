9f948ce3f991d2080edb5f96613aee6acd292cb0
KajaBraz <kajabraz@gmail.com>
2021-05-17 23:58:46 +0200

    sentiment analysis

--- TextAnalysis_Sentiment_Topic/prepare_url_text.py
+++ TextAnalysis_Sentiment_Topic/prepare_url_text.py
@@ -1,5 +1,6 @@
-from urllib import request
 from bs4 import BeautifulSoup
+from urllib import request
+from textblob import TextBlob
 
 
 def get_url_content(url: str) -> [str]:
@@ -11,11 +12,17 @@ def get_url_content(url: str) -> [str]:
     return strips
 
 
-def get_text(string_list: [str]) -> str:
-    return ' '.join(string_list)
+def get_text_and_lang(string_list: [str]) -> (str, str):
+    text = ' '.join(string_list)
+    blob = TextBlob(text)
+    lang = blob.detect_language()
+    return text, lang
 
 
 if __name__ == '__main__':
-    content = get_url_content('https://www.tuttosport.com/news/calcio/coppa-italia/2021/05/13-81655553/'
-                              'juve-atalanta_la_finale_di_coppa_italia_con_4300_spettatori')
-    print(content)
+    my_url = 'https://www.tuttosport.com/news/calcio/coppa-italia/2021/05/13-81655553/' \
+             'juve-atalanta_la_finale_di_coppa_italia_con_4300_spettatori'
+    content = get_url_content(my_url)
+    print(content[:10])
+    my_text, text_lang = get_text_and_lang(content)
+    print(text_lang, my_text[:150])
--- TextAnalysis_Sentiment_Topic/text_analysis.py
+++ TextAnalysis_Sentiment_Topic/text_analysis.py
@@ -1,3 +1,4 @@
+from textblob import TextBlob
 from wordcloud import WordCloud
 import many_stop_words
 
@@ -13,9 +14,40 @@ def get_cloud(text: str, language_code: str = ''):
     wordcloud.to_file('cloud.png')
 
 
+def get_sentiment_scores(text: str) -> (float, float):
+    polarity, subjectivity = TextBlob(text).sentiment
+    return polarity, subjectivity
+
+
+def analyze_sentiment(polarity: float) -> str:
+    if polarity <= -0.5:
+        return 'negative'
+    elif -0.5 < polarity < -0.15:
+        return 'slightly negative'
+    elif -0.15 <= polarity <= 0.15:
+        return 'neutral'
+    elif 0.15 < polarity < 0.5:
+        return 'slightly positive'
+    return 'positive'
+
+
+def analyze_subjectivity(subjectivity: float) -> str:
+    if subjectivity >= 0.66:
+        return 'personal opinion'
+    elif 0.66 > subjectivity > 0.33:
+        return 'equilibrium between personal opinion and factual information'
+    return 'factual information'
+
+
 if __name__ == '__main__':
-    url = prepare_url_text.get_url_content(
-        'https://www.tuttosport.com/news/calcio/coppa-italia/2021/05/13-81655553/'
-        'juve-atalanta_la_finale_di_coppa_italia_con_4300_spettatori')
-    text = prepare_url_text.get_text(url)
-    get_cloud(text, 'it')
+    my_url = 'https://www.tuttosport.com/news/calcio/coppa-italia/2021/05/13-81655553/' \
+             'juve-atalanta_la_finale_di_coppa_italia_con_4300_spettatori'
+    content = prepare_url_text.get_url_content(my_url)
+    my_text, text_lang = prepare_url_text.get_text_and_lang(content)
+    get_cloud(my_text, text_lang)
+    text_polarity, text_subjectivity = get_sentiment_scores(my_text)
+    print(text_polarity, text_subjectivity)
+    sentiment = analyze_sentiment(text_polarity)
+    print(sentiment)
+    subjectivity_level = analyze_subjectivity(text_subjectivity)
+    print(subjectivity_level)
