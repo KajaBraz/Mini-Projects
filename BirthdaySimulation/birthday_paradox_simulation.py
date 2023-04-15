58e3d1adafe5347087b5e7ebd484f006a1dd4125
Kaja Brąz <kajabraz@gmail.com>
2023-04-15 02:27:17 +0200

    second approch added; logging removed

--- BirthdaySimulation/birthday_paradox_simulation.py
+++ BirthdaySimulation/birthday_paradox_simulation.py
@@ -1,43 +1,42 @@
 """
-A program which simulates groups of people, choosing randomly their members' birthday dates (day and month)
-and calculating which nth person will happen to have their birthday the same day as any other group member.
+A program (experiment) which takes two approaches to simulate the birthday paradox
+(https://en.wikipedia.org/wiki/Birthday_problem) which states that 23 people are enough in order to achieve
+an above 50% probability of at least two of them sharing their birthdays.
 
-The simulation estimates how many people is enough to ensure that 2 of them will have their birthday the same day.
+Approach 1
 
-The results are provided as the following figures:
-    - the median of a given number of groups (which means 50% probability that 2 people out of the result number have
-        birthdays the same day)
-    - the mean of a given number of groups
-    - the smallest number resulting from the simulation
-    - the highest number resulting from the simulation
+The first approach simulates a provided number of groups, choosing randomly their members' birthday dates and
+calculating which nth person will happen to have their birthday the same day as any other group member.
+The result of the simulation is represented by the median of the groups sizes.
+It is accompanied by the following figures: the mean, the smallest, and the biggest number of the group sizes.
 
-The details of the results are written into a log file.
+Approach 2
+
+The second approach takes a provided number of groups and their members. For each group, it is checked if there
+are at least any two members who happen to have their birthday the same day.
+
+Both approaches of the problem simulations show that it takes approximately 23 people to get 50% probability of
+the problem in question.
 """
 
-import datetime
-import logging
-import os
 import statistics
 from random import randrange
-from typing import Tuple
+from typing import List
 
 
-def draw_date() -> Tuple[int, int]:
+def draw_date() -> int:
     """
-    Draw a month and a day. It is assumed that February is 28 days long
-    :return: day and month
+    Draw one day out of a 365 days long year
+    :return: drawn day of a year
     """
 
-    months_days = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
-    month = randrange(1, 13)
-    day = randrange(1, months_days[month] + 1)
-    return day, month
+    days = 365
+    return randrange(days)
 
 
-def simulate_one_group(iteration: int) -> int:
+def simulate_one_group() -> int:
     """
     Simulate a group of people by adding new members until any two of them have their birthday on the same day
-    :param iteration: group's number
     :return: number of group members
     """
 
@@ -46,62 +45,82 @@ def simulate_one_group(iteration: int) -> int:
         birthday = draw_date()
         if birthday in birthdays:
             group_length = len(birthdays) + 1
-            logging.debug(f'\nGroup {iteration}'
-                          f'\n\tTwo people have birthday on the same day when the number of people in the group is:'
-                          f'\n\t{group_length}')
-            logging.debug(f'\tDates are written in (day, month) format.'
-                          f'\n\t{birthday} - {sorted(birthdays, key=lambda d: (d[1], d[0]))}')
             return group_length
         birthdays.add(birthday)
 
 
-def main(n: int) -> None:
+def run_n_simulations(n: int) -> List[int]:
     """
     Run simulations the provided number of times
     :param n: number of simulations
+    :return: scores for a given number of groups
     """
 
-    set_logger()
+    return [simulate_one_group() for i in range(1, n + 1)]
 
-    results = [simulate_one_group(i) for i in range(1, n + 1)]
-    results = sorted(results)
+
+def get_simulation_details(results: List[int]) -> None:
+    """
+    Print median, mean, min, and max, of the results of the Approach 1
+    :param results: scores for a given number of groups
+    :return: None
+    """
 
     average = sum(results) / len(results)
     median = statistics.median(results)
     min_result = min(enumerate(results), key=lambda x: x[1])
     max_result = max(enumerate(results), key=lambda x: x[1])
-    logging.info(
-        f'\nResults for {n} groups'
-        f'\n\tMedian: {median}\n\t\t- the group composed of this number of people has 50% chance that any two members'
+    print(
+        f'Median: {median} - the group composed of this number of people has 50% chance that any two members'
         f'have their birthdays the same day'
-        f'\n\tMean: {average}'
-        f'\n\tMin: {min_result[1]} - group {min_result[0]}'
-        f'\n\tMax: {max_result[1]} - group {max_result[0]}'
+        f'\nMean: {average}'
+        f'\nMin: {min_result[1]} - group {min_result[0]}'
+        f'\nMax: {max_result[1]} - group {max_result[0]}\n'
     )
-    close_logger()
 
 
-def set_logger() -> None:
+def simulate_n_people_group(people: int) -> bool:
     """
-    Set log file
-    :return: None
+    Simulate a group of n people and check if there are at least two of them who have their birthdays the same day
+    :param people: number of people in a group
+    :return: boolean value indicating if the condition is satisfied
     """
 
-    date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    folder = 'birthday_simulation_logs'
-    path = os.path.join(folder, f'birthdays_{date}.logs')
-    os.makedirs(folder, exist_ok=True)
-    logging.basicConfig(filename=path, level=logging.DEBUG, format='%(message)s')
-    stream = logging.StreamHandler()
-    stream.setLevel(logging.INFO)
-    logging.getLogger('').addHandler(stream)
-    print(f'\nLog file was created in {path}. The details of the simulation can be found there.')
+    birthdays = set()
+    birthday = draw_date()
+    while birthday not in birthdays and len(birthdays) < people:
+        birthdays.add(birthday)
+        birthday = draw_date()
+    return birthday in birthdays
 
 
-def close_logger():
-    logging.shutdown()
+def get_probability_for_n_people(people: int, n_groups: int) -> float:
+    """
+    Calculate the probability of at least two group members having their birthdays the same day. The estimation is done
+     for a given number of groups, each of them having a specified number of members
+    :param people: number of people in a group
+    :param n_groups: number of groups
+    :return: probability calculated for given conditions
+    """
+
+    results = [simulate_n_people_group(people) for _ in range(n_groups)]
+    return len([x for x in results if x]) / n_groups
 
 
 if __name__ == "__main__":
+    # Approach 1
+    print('Approach 1')
     simulated_groups = 1000000
-    main(simulated_groups)
+    simulation_results = run_n_simulations(simulated_groups)
+    get_simulation_details(simulation_results)
+
+    # Approach 2
+    print('Approach 2')
+    simulated_groups = 1000000
+    group_members = 23
+    probability = get_probability_for_n_people(group_members, simulated_groups)
+    print(
+        f'For given conditions:\n\tNumber of groups: {simulated_groups}'
+        f'\n\tNumber of group members: {group_members}'
+        f'\nThe probability of at least two members having their birthdays the same day is: {probability}'
+    )
